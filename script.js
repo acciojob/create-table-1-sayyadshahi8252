be9be0872e565c6e6b8a5c51a3001a5f973d6809
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-26 17:15:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+	let celladded=document.getElementById("sampleTable")
+	celladded.insertAdjacentHTML("afterbegin", `
+  <tr>
+    <td>New cell1</td>
+    <td>New cell2</td>
+  </tr>
+`);
   
 }
