a44a948ab3dd576c9c49db5060177535e99e5e4f
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-26 17:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function insert_Row() {
-    //Write your code here
-	let celladded=document.getElementById("sampleTable")
-	celladded.insertAdjacentHTML("afterbegin", `
-  <tr>
-    <td>New Cell1</td>
-    <td>New Cell2</td>
-  </tr>
-`);
-  
+  const table = document.getElementById("sampleTable");
+  const newRow = table.insertRow(0); // insert at top (index 0)
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  cell1.innerText = "New Cell1";
+  cell2.innerText = "New Cell2";
 }
